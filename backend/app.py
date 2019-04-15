265910ba3dad5cc28e83f3de87e61d4b9449c032
Venkat Pantham <venkatpantham5568@gmail.com>
2019-04-16 02:12:20 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -4,7 +4,7 @@ app=Flask(__name__)
 
 @app.route('/api/pokemon/<id>')
 def id(id):
-    if int(id) < 808:
+    if int(id) <= 151:
         data=requests.get('https://pokeapi.co/api/v2/pokemon-form/'+id).json()
         num=data['id']
         name=data['name']
@@ -12,7 +12,7 @@ def id(id):
         front_default=sprites['front_default']
         return jsonify({'id':num,'name':name,'sprite':front_default})
     else:
-        return ('Pokemon not found,Sorry We have only 807 types of Pokemons!')
+        return ('Pokemon not found!')
 
 if __name__ == '__main__':
     app.run(host='localhost',port=8006,debug=True)
