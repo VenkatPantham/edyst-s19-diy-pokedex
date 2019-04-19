994d8654af54db05d9107941d94aed4aabb1f848
VenkatPantham <venkatpantham5568@gmail.com>
2019-04-20 00:54:03 +0530

    Error 404 Handled

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,12 @@
 # API service for Pokemon data
-The backend folder consists of app.py,requirements.txt
-This api service is used to display pokemon data with the id
+The backend folder consists of app.py,requirements.txt,pokemon_data.json
+
+app.py:
+    This file is an used as API service which is used to display the pokemon data according to the id from the pokemon_data.json file
+
+pokemon_data.json:
+    This file consists of all the pokemon data in JSON format which is used by app.py 
+
+requirements.txt:
+    This file consists of all the requirements which are used by the API service
+    All these requriements should be installed in order to run the application
--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,18 @@
-from flask import Flask,redirect,jsonify,json
-import requests
+from flask import Flask,redirect,jsonify,json,render_template,Response
 app=Flask(__name__)
 
-@app.route('/api/pokemon/<id>')
-def id(id):
-    if int(id) <= 151 and int(id)>=1:
-        data=requests.get('https://pokeapi.co/api/v2/pokemon-form/'+id).json()
-        num=data['id']
-        name=data['name']
-        sprites=data['sprites']
-        front_default=sprites['front_default']
-        return jsonify({'id':num,'name':name,'sprite':front_default})
-    else:
-        return ('Pokemon not found!')
+@app.errorhandler(404)
+def error(e):
+    return render_template('404.html'),404
 
+@app.route('/api/pokemon/<int:id>')
+def id(id):
+    if(id>0 and id<152):
+        json_data=open('pokemon_data.json')
+        data=json.load(json_data)
+        pokemon=data[id-1]
+        return jsonify(pokemon)
+    else:
+        return('<h1>404 Page Not Found</h1>')
 if __name__ == '__main__':
     app.run(host='localhost',port=8006,debug=True)
