ab1877d8a53370a43e93ef22cab509485c0801df
VenkatPantham <venkatpantham5568@gmail.com>
2019-05-04 21:26:13 +0530

    formatted with black

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,25 @@
-from flask import Flask,jsonify,render_template,json
-app=Flask(__name__)                                                 # Initializing Flask
+from flask import Flask, jsonify, render_template, json
 
-@app.errorhandler(404)                                              # Handling 404 Error
+app = Flask(__name__)  # Initializing Flask
+
+
+@app.errorhandler(404)  # Handling 404 Error
 def page_not_found(e):
-    return render_template('404.html'),404
+    return render_template("404.html"), 404
 
-@app.route('/api/pokemon/<int:id>')                                 
+
+@app.route("/api/pokemon/<int:id>")
 def id(id):
-    if(id>0 and id<152):
-        poke={}
-        data=json.load(open('pokemon_data.json'))                   # Opening and Loading Pokemon Data JSON File
-        poke['pokemon']=data[id-1]                                  # Storing all the data in a variable
-        return json.dumps(poke)                                     # Displaying required JSON data
+    if id > 0 and id < 152:
+        poke = {}
+        data = json.load(
+            open("pokemon_data.json")
+        )  # Opening and Loading Pokemon Data JSON File
+        poke["pokemon"] = data[id - 1]  # Storing all the data in a variable
+        return json.dumps(poke)  # Displaying required JSON data
     else:
-        return render_template('404.html'),404
+        return render_template("404.html"), 404
 
-if __name__ == '__main__':
-    app.run(host='localhost',port=8006)
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8006)
